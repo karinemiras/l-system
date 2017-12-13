98602d986d0ca7fab12c8dcb0f4d748ed60c2f77
karinemiras <karine.smiras@gmail.com>
2017-12-13 20:16:02 +0100

    small changes

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/karinemiras/projects/coevolution-revolve/l-system
-# Build directory: /Users/karinemiras/projects/coevolution-revolve/l-system/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lsystem")
